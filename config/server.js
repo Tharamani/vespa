48579db6f9c69268ef8047959ab930ade78310da
Tharamani <tharamani15@gmail.com>
2022-08-01 10:28:34 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c190b80e42bbb4e88682bcd381e14a28'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a6868014-d5f3-4c5b-94da-1b4df66e3b94'
+};
